4c3b0f0d9d35123195f25fcf8d28aae073038c32
FIRAT AKSU <aksu_fr@yahoo.es>
2018-03-17 07:43:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments){
+  var arr = []
+  for(var i=0, mscsLen=musicians.length; i < mscsLen; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts(facts){
+ var i=0
+ var factsLen = facts.length
+ while(i < factsLen){
+   facts[i] = facts[i].concat("!!!")
+   i++
+ }
+ return facts
+}
+
+function iLoveTheBeatles(num){
+  var arr = []
+  do{
+    arr.push("I love the Beatles!")
+  }while(num < 15);
+  
+  if(num > 14){  
+    arr.push("I love the Beatles!")
+    return arr
+  }
+  else return arr
+}
